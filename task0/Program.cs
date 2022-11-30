8ffac928af87b3811517a4bd19adad59c5bc729c
puzz <taste@inbox.ru>
2022-11-30 16:05:42 +0300

    New homework done

---
+++ task0/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 28: Напишите программу,
+ которая принимает на вход число N 
+ и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120*/
+
+int degree(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i<= numberB; i++)
+    {
+        result = result * numberA; 
+    }    
+    return result;
+}
+int numberA = new int();
+Console.WriteLine("Введите число");
+numberA = Convert.ToInt32(Console.ReadLine());
+ int numberB = new int();
+Console.WriteLine("Введите  степень");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+    int result = degree(numberA,numberB);
+    Console.WriteLine($" Число  {numberA} в {numberB} степени = {result}");
+
---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 27: Напишите программу, которая принимает 
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int userNumber = new int();
+Console.WriteLine("Введите число");
+userNumber = Convert.ToInt32(Console.ReadLine());
+
+int sunOfNumbers (int userNumber)
+{
+    int result = 0;
+    for (int i = userNumber; i > 0;)
+    {
+        result = result + i % 10;
+        i = i / 10;
+    }
+    return result;
+}
+int result = sunOfNumbers(userNumber);
+Console.WriteLine ($" Сумма цифр в числе {userNumber} = {result}");
